9f4eb441c406d3b0d4a2e41f12309b75ce73d81a
anthonymujic <anthony.mujic@gmail.com>
2023-08-13 01:00:26 +1000

    Added a test

--- test/htmex_test.exs
+++ test/htmex_test.exs
@@ -1,8 +1,17 @@
-defmodule HtmexTest do
+defmodule HTMeXTest do
   use ExUnit.Case
-  doctest Htmex
+  doctest HTMeX
+  import HTMeX
 
   test "greets the world" do
-    assert Htmex.hello() == :world
+    conn =
+      Plug.Adapters.Test.Conn.conn(%Plug.Conn{}, :get, "/", %{})
+      # conn =
+      #   Plug.Conn.__struct__()
+      |> Phoenix.Controller.put_root_layout(html: {AppView, :root})
+      |> htmex("opts")
+      |> IO.inspect()
+
+    assert Phoenix.Controller.root_layout(conn) == {AppView, :root}
   end
 end
